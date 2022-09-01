05b8c2386b44b24cedbbb830a74ad2f6ef845ca2
Prathamesh <prathamesh@inviziosolutions.com>
2022-09-01 16:48:54 +0530

    Shared Preferences Demo

---
+++ app/src/main/java/com/example/sharedpreferencesdemo/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.sharedpreferencesdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import splitties.activities.start
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        lifecycleScope.launch {
+            delay(2000)
+            start<LoginActivity>()
+        }
+    }
+}
